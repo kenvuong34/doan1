a136489009354440e3b7dee2e8dbe7652fb176f0
Ni <120183605+anhquynhanh@users.noreply.github.com>
2024-03-28 10:51:32 +0700

    Update pay.js

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -3,10 +3,10 @@ const transButton = document.querySelector('.trans button');
 
 cashButton.addEventListener('click', function() {
     alert('Please pay the money when receiving the goods.');
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
 
 transButton.addEventListener('click', function() {
     alert('After successful transfer, please take the transaction and send it to Zalo: (+84) 829 400 390 for us to confirm the application.');
-    window.location.href = 'home.html';
+    window.location.href = 'index.html';
 });
